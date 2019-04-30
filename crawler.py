4501ea58ba27f036fdeb1929e491e61e83ee8bf5
Chun-Yi Kuo <kingrobber@gmail.com>
2019-04-30 23:41:01 +0800

    1. add json parser 2. add criteria_neg

--- crawler.py
+++ crawler.py
@@ -1,8 +1,9 @@
 import requests
 import pandas as pd
+import json
 from io import StringIO
 
-def crawlCriticalInformation():
+def crawlCriticalInformation(parse_to_json = False):
     res = requests.get('http://mops.twse.com.tw/mops/web/ajax_t05sr01_1')
     res.encode = 'utf-8'
     dfs = pd.read_html(StringIO(res.text))
@@ -10,12 +11,17 @@ def crawlCriticalInformation():
     ret = pd.DataFrame()
     # code, name, date, content
 
-    criteria = ['財務報', '盈餘']
+    criteria_pos = ['財務報', '盈餘']
+    criteria_neg = ['比率']
 
     for index in range(0, len(dfs[1])):
         match = False
-        for cr in criteria:
-            match = match or ( dfs[1].iloc[index]['主旨'].find(cr) != -1)
+        for crp in criteria_pos:
+            match = match or ( dfs[1].iloc[index]['主旨'].find(crp) != -1)
+            for crn in criteria_neg:
+                if dfs[1].iloc[index]['主旨'].find(crn) != -1:
+                    match = False
+
             if(match):
                     try:
                         tmp = dfs[1].iloc[index]
@@ -23,4 +29,25 @@ def crawlCriticalInformation():
                     except Exception as err:
                         print(err)
                     break      
+    if parse_to_json:
+        colHeader = list(ret.columns.values)
+        colHeader.pop(0)
+        rowHeader = list(ret.index)
+        #print(dfs.loc[rowHeader[1]])
+
+        dataArr = []
+
+        for i in rowHeader:
+            try:
+                tmpDict = {}
+                for k in colHeader:
+                    if k == '公司代號':
+                        tmpDict[k] = str(int(ret.loc[i][k]))
+                    else:
+                        tmpDict[k] = ret.loc[i][k]
+                dataArr.append(tmpDict)
+            except Exception as err:
+                print(err)
+
+        ret = json.dumps(dataArr)
     return ret
