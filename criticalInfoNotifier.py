bddeed1b764698694b1319c1b804844dc606e33e
vagrant <vagrant@vagrant.vm>
2021-02-20 16:20:24 +0800

    [feat] add function crawlDataUseBs4 for compose notification url link

--- criticalInfoNotifier.py
+++ criticalInfoNotifier.py
@@ -1,10 +1,14 @@
 from crawler import crawlCriticalInformation as info
+from crawler import crawlDataUseBs4
 from telegramParser import sendCriticalInfo as infoSender
 
-data = info(False)
+# data = info(False)
+data = crawlDataUseBs4()
 #print(len(data))
-#print(data)
+# print(data)
 if len(data) == 0:
     print("No critical info.")
 else:
-    infoSender(data)
+    pass
+    #print(data)
+    #infoSender(data)
