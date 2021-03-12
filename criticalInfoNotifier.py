4521dc3dd46e29b3400f8c878371b1167f7157b7
cykuotw <cykuotw45@gmail.com>
2021-03-12 21:23:22 +0800

    [refactor] adapt crawler bs4

--- criticalInfoNotifier.py
+++ criticalInfoNotifier.py
@@ -1,14 +1,17 @@
 from crawler import crawlCriticalInformation as info
 from crawler import crawlDataUseBs4
+from crawler import filterKeyword
 from telegramParser import sendCriticalInfo as infoSender
 
 # data = info(False)
 data = crawlDataUseBs4()
-#print(len(data))
+data = filterKeyword(data)
+
+# print(len(data))
 # print(data)
 if len(data) == 0:
     print("No critical info.")
 else:
-    pass
-    #print(data)
-    #infoSender(data)
+    # pass
+    # print(len(data))
+    infoSender(data, False)
