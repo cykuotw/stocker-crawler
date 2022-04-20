669685e7ad06b2fe6eb7468a65bb18bd6e281408
ckyuo <cykuotw45@gmail.com>
2022-04-20 15:38:15 -0400

    [refactor] rename script

--- runCriticalNotifier.sh
+++ runCriticalNotifier.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /home/ec2-user/project/stockerCrawler
+# cd /home/ec2-user/project/stockerCrawler
+cd ~/stocker-crawler
 source venv/bin/activate
 python3 criticalInfoNotifier.py
 deactivate
