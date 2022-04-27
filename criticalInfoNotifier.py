85192066dccf234b4d151a1a3fdb4174a712438d
cykuo <cykuotw45@gmail.com>
2022-04-27 15:19:34 -0400

    [feat] add slack notification

--- criticalInfoNotifier.py
+++ criticalInfoNotifier.py
@@ -2,15 +2,25 @@ from datetime import datetime
 
 from crawler.core.criticalInfo import crawlDataUseBs4
 from notifier.notifierUtil import (filterKeyword, infoSender,
-                                   postStockerAnnouncement)
+                                   postStockerAnnouncement, pushSlackMessage)
 
 # data = info(False)
-data = crawlDataUseBs4()
-data = filterKeyword(data)
+curTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+pushSlackMessage("Stocker每日重訊", '{} crawler work start.'.format(curTime))
 
-if len(data) == 0:
-    print("No critical info.")
-else:
-    postStockerAnnouncement(data)
-    if datetime.now().hour >= 21 and datetime.now().hour <= 22:
-        infoSender(data, False)
+try:
+    data = crawlDataUseBs4()
+    data = filterKeyword(data)
+
+    if len(data) == 0:
+        print("No critical info.")
+    else:
+        postStockerAnnouncement(data)
+        if datetime.now().hour >= 21 and datetime.now().hour <= 22:
+            infoSender(data, False)
+except Exception as e:
+    curTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    pushSlackMessage("Stocker每日重訊", '{} crawler error.'.format(curTime, e))
+finally:
+    curTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    pushSlackMessage("Stocker每日重訊", '{} crawler work done.'.format(curTime))
