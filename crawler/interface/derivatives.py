2db524c606e9df29d130ec1e1211c193f393fe47
cykuo <cykuotw45@gmail.com>
2023-05-31 16:32:50 -0400

    [feat] add updateDailyVolatility

--- crawler/interface/derivatives.py
+++ crawler/interface/derivatives.py
@@ -1,9 +1,10 @@
 import json
-import requests
-import math
 
-from crawler.core.derivatives import crawlStockFuture
+import requests
+
+from crawler.core.derivatives import crawlStockFuture, crawlVolatility
 from crawler.interface.util import stockerUrl
+from notifier.discord import pushDiscordVolatility
 
 
 def updateStockCommodity():
@@ -24,13 +25,29 @@ def updateStockCommodity():
         if row["標準型證券股數"] in [2000, 100]:
             if row["標準型證券股數"] == 2000:
                 dataPayload["stock_future"] = isinstance(row["是否為股票期貨標的"], str)
-                dataPayload["stock_option"] = isinstance(row["是否為股票選擇權標的"], str)
+                dataPayload["stock_option"] = isinstance(
+                    row["是否為股票選擇權標的"], str)
             elif row["標準型證券股數"] == 100:
-                dataPayload["small_stock_future"] = isinstance(row["是否為股票期貨標的"], str)
+                dataPayload["small_stock_future"] = isinstance(
+                    row["是否為股票期貨標的"], str)
 
             dataPayload["stock_id"] = row["證券代號"]
             print(dataPayload)
             res = requests.post(
-                    "{}/{}".format(
-                        serverStockCommodityApi, dataPayload['stock_id']),
-                    data=json.dumps(dataPayload))
+                "{}/{}".format(
+                    serverStockCommodityApi, dataPayload['stock_id']),
+                data=json.dumps(dataPayload))
+
+
+def updateDailyVolatility() -> None:
+    data = crawlVolatility()
+
+    message = "**" + data['商品'] + "**\n\t"
+    message += "成交價: " + data['成交價'] + "\t"
+    message += "漲跌: " + data['漲跌'] + "\t"
+    message += "震幅: " + data['震幅'] + "\n\t"
+    message += "**點數差: " + data['點數差'] + "**\t"
+    message += "最高: " + data['最高'] + "\t"
+    message += "最低: " + data['最低']
+
+    pushDiscordVolatility("每日台指波動", message)
