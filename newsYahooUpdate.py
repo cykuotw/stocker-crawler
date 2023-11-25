ec204e58fe076317c509fd58b0495f7d4a4e4d8f
cykuo <cykuotw45@gmail.com>
2023-11-25 15:47:31 -0500

    [fix] add run script

---
+++ newsYahooUpdate.py
@@ -0,0 +1,4 @@
+from crawler.interface.news import updateDailyNewsYahoo
+
+if __name__ == '__main__':
+    updateDailyNewsYahoo()
--- runUpdateYahoo.sh
+++ runUpdateYahoo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(dirname "$0")
 source venv/bin/activate
-python3 updateDailyNewsYahoo.py
+python3 newsYahooUpdate.py
 deactivate
 exit 0
