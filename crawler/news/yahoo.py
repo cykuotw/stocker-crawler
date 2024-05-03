46d7cff2865400d3c4b53cd07041b7dd894a7874
cykuo <cykuotw45@gmail.com>
2024-05-02 22:39:45 -0400

    refactor: serverless yahoo crawler

---
+++ crawler/news/yahoo.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+from time import sleep
+
+import feedparser
+import requests
+
+from crawler.common.notifier import pushNewsMessge
+from crawler.common.util.config import YAHOO
+from crawler.common.util.server import getStockNoBasicInfo, updateNewsToServer
+
+
+def crawlNewsYahoo(companyID: str = '2330'):
+    """
+    @Description:
+        爬取Yahoo Stock個股每日新聞\n
+        Crawl daily news of specific companyID form Yahoo Stock\n
+    @Param:
+        companyID => string (default: '2330')
+    @Return:
+        json (see example) (empty if companyID not valid)
+    """
+
+    headers = {
+        'User-Agent': ("Mozilla/5.0 "
+                       "(Macintosh; Intel Mac OS X 10_10_1) "
+                       "AppleWebKit/537.36 (KHTML, like Gecko) "
+                       "Chrome/39.0.2171.95 Safari/537.36"),
+        'Content-Type': 'text/xml;'
+    }
+
+    url = f"https://tw.stock.yahoo.com/rss?s={companyID}"
+
+    waitTime = 0.1
+    feed = None
+    for _ in range(5):
+        rsp = requests.get(url, headers, timeout=10)
+
+        # if status code is not 200 ok
+        # retry 5 times max, each time extends wait time by 2x
+        if rsp.status_code != 200:
+            sleep(waitTime)
+            waitTime *= 2
+            continue
+
+        feed = feedparser.parse(rsp.text)
+        break
+
+    if feed is None:
+        return {}
+
+    data = []
+    for item in feed['entries']:
+        publishTime = datetime.strptime(
+            item['published'], '%a, %d %b %Y %H:%M:%S %Z')
+        tmp = {}
+        tmp['link'] = item['link']
+        tmp['stocks'] = [companyID]
+        tmp['title'] = item['title']
+        tmp['source'] = 'yahoo'
+        tmp['releaseTime'] = publishTime.isoformat()
+        tmp['feedType'] = "news"
+        tmp['tags'] = []
+        tmp['description'] = item['summary']
+
+        data.append(tmp)
+
+    result = {}
+    result['data_count'] = len(data)
+    result['data'] = data
+
+    return result
+
+
+def updateDailyNewsYahoo():
+    """
+    @Description:
+        更新每日Yahoo新聞\n
+        Update all daily news related to tw stock market
+        from yahoo to stocker server\n
+    @Param:
+        N/A
+    @Return:
+        N/A
+    """
+    totalSlices = YAHOO['total-slices']
+    currentSlices = YAHOO['current-slices']
+
+    pushNewsMessge(f"Yahoo crawler ({currentSlices}/{totalSlices}) start")
+    try:
+        idList = getStockNoBasicInfo()
+        start = round(len(idList) * (currentSlices-1) / totalSlices)
+        end = round(len(idList) * (currentSlices) / totalSlices)
+        idList = idList[start:end]
+
+        for _, stockId in enumerate(idList):
+            news = crawlNewsYahoo(str(stockId))
+            updateNewsToServer(news)
+
+    except Exception as ex:
+        pushNewsMessge(f"Yahoo crawler error: {ex}")
+
+    pushNewsMessge(f"Yahoo crawler ({currentSlices}/{totalSlices}) done")
