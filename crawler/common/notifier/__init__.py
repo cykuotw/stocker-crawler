100c9ec5d4f40195090500ba2fce6625ededf055
cykuo <cykuotw45@gmail.com>
2024-08-16 22:47:34 -0400

    feat: add pushCriticalInfoMessage to notifier

--- crawler/common/notifier/__init__.py
+++ crawler/common/notifier/__init__.py
@@ -22,3 +22,24 @@ def pushNewsMessge(message: str = ""):
     discord.pushLog(
         "Stocker每日新聞",
         f"{datetime.now().strftime('%m/%d/%Y, %H:%M:%S')} {message}")
+
+
+def pushCriticalInfoMessage(message: str = ""):
+    """
+    @Description:
+        推送log\n
+        push log messges\n
+    @Param:
+        message => str (default: "")
+    @Return:
+        N/A
+    """
+    if message == "":
+        return
+
+    slack.push(
+        "Stocker每日重訊",
+        f"{datetime.now().strftime('%m/%d/%Y, %H:%M:%S')} {message}")
+    discord.pushLog(
+        "Stocker每日重訊",
+        f"{datetime.now().strftime('%m/%d/%Y, %H:%M:%S')} {message}")
